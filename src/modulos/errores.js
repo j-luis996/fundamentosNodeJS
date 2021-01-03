5ed3d26e4e31a6a6ad1eff67c0321f6fbc08f993
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-03 16:06:50 -0600

    clase de errores (try/cathc)

---
+++ src/modulos/errores.js
@@ -0,0 +1,13 @@
+function otraFuncion(){
+      seRompe()
+}
+function seRompe(){
+      return 3 + z 
+}
+try{
+      otraFuncion()
+}catch(err){
+      console.error('algo se rompio')
+      console.error(err.message)
+}
+console.log('esto esta despues de try')
