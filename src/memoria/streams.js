5c2b2793d55b7a5974fdc95b1dcc6994ef8f39a1
Luis Martinez <s_k_ap3@hotmail.com>
2021-01-09 02:12:30 -0600

    clase de Streams

---
+++ src/memoria/streams.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const stram = require('stream');
+const util = require('util');
+
+
+let data='';
+
+let readableStream = fs.createReadStream(__dirname+'/input.txt');
+/*
+readableStream.setEncoding('utf-8');
+
+readableStream.on('data', function (chunk){
+      data+=chunk;
+});
+
+readableStream.on('end', function (){
+      console.log(data);
+});
+process.stdout.write('XD');
+      */
+const Transform = stram.Transform;
+
+function Mayus(){
+      Transform.call(this);
+}
+util.inherits(Mayus,Transform);
+
+Mayus.prototype._transform = function (chunk, codif, cb){
+      chunkMayus=chunk.toString().toUpperCase();
+      this.push(chunkMayus);
+      cb();
+}
+
+let mayus = new Mayus();
+
+readableStream.pipe(mayus).pipe(process.stdout);
